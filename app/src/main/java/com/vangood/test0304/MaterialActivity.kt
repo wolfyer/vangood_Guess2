9e52ba6222049e3d82ac505af25d1e973bb66101
Class <a90023y@gmail.com>
2022-03-08 14:13:06 +0800

    save0308

--- app/src/main/java/com/vangood/test0304/MaterialActivity.kt
+++ app/src/main/java/com/vangood/test0304/MaterialActivity.kt
@@ -4,21 +4,14 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AlertDialog
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.vangood.test0304.databinding.ActivityMaterialBinding
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.ed_num
 import kotlinx.android.synthetic.main.activity_material.*
-import kotlinx.android.synthetic.main.content_material.*
+
 
 class MaterialActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMaterialBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,29 +22,22 @@ class MaterialActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_material)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
 
         binding.fab.setOnClickListener { view ->
             AlertDialog.Builder(this)
                 .setTitle("Replay Game")
                 .setMessage("Are you sure?")
-                .setPositiveButton(getString(R.string.ok), { dialog, which ->
+                .setPositiveButton(getString(R.string.ok)){ dialog, which ->
                     secretNumber.reset()
                     counter.setText(secretNumber.count.toString())
-                })
+                }
                 .setNegativeButton("Cancel",null)
                 .show()
         }
         counter.setText(secretNumber.count.toString())
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_material)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
     val secretNumber = SecretNumber()
     fun go(view: View){
         val n = ed_num.text.toString().toInt()
